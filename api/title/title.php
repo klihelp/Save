61671e336b42dce71f52f727647bc8fdf6878530
Max <hi@krmax44.de>
2017-01-07 16:30:44 +0100

    Added caching for titles
    Save is now way faster. Check out the Changelog to learn more.

--- api/title/title.php
+++ api/title/title.php
@@ -1,8 +1,25 @@
 <?php
 require "../start.php";
-error_reporting(E_ALL);
-$data = file_get_contents($_GET["url"]);
-$url = explode("<title>", utf8_encode($data));
-$str = explode("</title>", $url[1])[0];
-echo utf8_decode($str);
+if (file_exists("cache/".sha1($_GET["url"]))) {
+	echo file_get_contents("cache/".sha1($_GET["url"]));
+}
+else {
+	$data = file_get_contents($_GET["url"]);
+	$url = explode("<title>", utf8_encode($data));
+	$str = explode("</title>", $url[1])[0];
+	echo utf8_decode($str);
+
+	$handle = fopen("cache/".sha1($_GET["url"]), "w");
+	fwrite($handle, utf8_decode($str));
+	fclose($handle);
+}
+
+foreach (new DirectoryIterator("cache") as $fileInfo) {
+    if ($fileInfo->isDot()) {
+    	continue;
+    }
+    if (time() - $fileInfo->getCTime() >= 7*24*60*60) {
+        unlink($fileInfo->getRealPath());
+    }
+}
 ?>
