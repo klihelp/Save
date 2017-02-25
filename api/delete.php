ab52863b978595907ad13d85344c5c79ec065153
Max <hi@krmax44.de>
2017-02-25 16:08:37 +0100

    Added return values for delete.php

--- api/delete.php
+++ api/delete.php
@@ -1,4 +1,10 @@
 <?php
 require "start.php";
 mysqli_query($db, "DELETE FROM urls WHERE id = ".mysqli_real_escape_string($db, $_GET["url"]));
+if (mysqli_affected_rows($db) == 0) {
+	echo "error";
+}
+else {
+	echo "ok";
+}
 ?>
